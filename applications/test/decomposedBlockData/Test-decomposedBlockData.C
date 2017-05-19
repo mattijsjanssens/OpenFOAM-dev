e866b86e34d8f6a8243444818e462e76c1bb6b38
Mattijs Janssens <mattijs.janssens@gmail.com>
2017-05-19 17:32:10 +0100

    ENH: decomposedBlockData: stand-alone splitting

--- applications/test/decomposedBlockData/Test-decomposedBlockData.C
+++ applications/test/decomposedBlockData/Test-decomposedBlockData.C
@@ -32,6 +32,7 @@ Description
 #include "Time.H"
 #include "polyMesh.H"
 #include "decomposedBlockData.H"
+#include "OFstream.H"
 
 using namespace Foam;
 
@@ -43,8 +44,6 @@ int main(int argc, char *argv[])
     #include "setRootCase.H"
     #include "createTime.H"
     #include "createPolyMesh.H"
-
-    Info<< "End\n" << endl;
 
     IOobject io
     (
@@ -71,12 +70,83 @@ int main(int argc, char *argv[])
 //        data.write();
 //    }
 
-    // Read
     {
+        // Read
         io.readOpt() = IOobject::MUST_READ;
-        decomposedBlockData data(io);
+        decomposedBlockData data(Pstream::worldComm, io);
 
-        DebugVar(data);
+DebugVar(data);
+
+        // Scatter header information
+        word name(data.name());
+        Pstream::scatter(name);
+        data.rename(name);
+        Pstream::scatter(data.headerClassName());
+        Pstream::scatter(data.note());
+        //Pstream::scatter(data.instance());
+        //Pstream::scatter(data.local());
+
+        data.instance() = "123";
+
+        Pout<< "data.objectPath():" << data.objectPath() << endl;
+
+        fileName objPath
+        (
+            fileHandler().objectPath(data, decomposedBlockData::typeName)
+        );
+
+        fileName objDir(objPath.path());
+        if (!objDir.empty())
+        {
+            fileHandler().mkDir(objDir);
+            OFstream os
+            (
+                objPath,
+                IOstream::BINARY,
+                IOstream::currentVersion,
+                runTime.writeCompression()
+            );
+            if (!os.good())
+            {
+                FatalErrorInFunction<< "Problem opening " << os.name()
+                    << exit(FatalError);
+            }
+
+            if (!Pstream::master() && !data.IOobject::writeHeader(os))
+            {
+                FatalErrorInFunction<< "Failed writing header to " << os.name()
+                    << exit(FatalError);
+            }
+
+            //if (!data.writeData(os))
+            //{
+            //    FatalErrorInFunction<< "Failed writing data to " << os.name()
+            //        << exit(FatalError);
+            //}
+            //os << data;
+            //os.write
+            //(
+            //    reinterpret_cast<const char*>(data.cbegin()),
+            //    data.byteSize()
+            //);
+            string str
+            (
+                reinterpret_cast<const char*>(data.cbegin()),
+                data.byteSize()
+            );
+            os.writeQuoted(str, false);
+
+            if (!Pstream::master())
+            {
+                IOobject::writeEndDivider(os);
+            }
+
+            if (!os.good())
+            {
+                FatalErrorInFunction<< "Failed writing to " << os.name()
+                    << exit(FatalError);
+            }
+        }
     }
 
     return 0;
